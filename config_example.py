8ddb343323a8c0417ae562793fb726fa7177ba05
samsunghappytree123 <samsunghappytree123@naver.com>
2022-05-19 18:03:05 +0900

    feat(config_example): add config example

---
+++ config_example.py
@@ -0,0 +1,4 @@
+class app:
+    client_id = 123456789123456789 # 디스코드 클라이언트 ID를 입력해주세요. (int)
+    client_secret = ""  # 디스코드 클라이언트 시크릿 키를 입력해주세요. (str)
+    redirect_uri = ""  # 디스코드 OAUTH2 인증 후 리다이렉트할 URL 입력해주세요. (str)
